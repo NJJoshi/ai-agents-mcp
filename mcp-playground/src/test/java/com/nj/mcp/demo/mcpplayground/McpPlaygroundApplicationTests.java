85f966e13b4e8f36c932ef615152bdf929d4f108
Nirav Joshi <joshi.nirav21@gmail.com>
2026-06-21 10:30:00 +0530

    MCP Tool: Sample Test case

---
+++ mcp-playground/src/test/java/com/nj/mcp/demo/mcpplayground/McpPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nj.mcp.demo.mcpplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
