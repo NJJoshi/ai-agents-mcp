15e7a6a10bcc2603d04d23534de8648571dd9a09
Nirav Joshi <joshi.nirav21@gmail.com>
2026-06-29 16:38:27 +0530

    MCP Tool demo: Assistant-Driven decisions : Advanced error handling demo. host package classes

---
+++ mcp-playground/src/main/java/com/nj/mcp/demo/mcpplayground/sec05/host/controller/McpController.java
@@ -0,0 +1,39 @@
+package com.nj.mcp.demo.mcpplayground.sec05.host.controller;
+
+import com.nj.mcp.demo.mcpplayground.sec04.host.dto.ChatRequest;
+import com.nj.mcp.demo.mcpplayground.sec04.host.dto.McpSessionManifest;
+import com.nj.mcp.demo.mcpplayground.sec05.dto.UserCategory;
+import com.nj.mcp.demo.mcpplayground.sec05.dto.UserContext;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/mcp")
+public class McpController {
+    private final ChatClient client;
+    private final McpSessionManifest mcpSessionManifest;
+
+    public McpController(ChatClient client, McpSessionManifest mcpSessionManifest) {
+        this.client = client;
+        this.mcpSessionManifest = mcpSessionManifest;
+    }
+
+    @GetMapping("manifest")
+    public McpSessionManifest mcpSessionManifest(){
+        return this.mcpSessionManifest;
+    }
+
+    @PostMapping
+    public Flux<String> chat(@RequestBody ChatRequest request, @RequestHeader("userId") Integer userId) {
+        var userCategory = userId == 2 ? UserCategory.PREMIUM : UserCategory.STANDARD;
+        return this.client.prompt(request.message())
+                .advisors(spec -> spec.param(ChatMemory.CONVERSATION_ID, userId))
+                .toolContext(Map.of("userContext", new UserContext(userId, userCategory)))
+                .stream()
+                .content();
+    }
+}
---
+++ mcp-playground/src/main/java/com/nj/mcp/demo/mcpplayground/sec05/host/dto/ChatRequest.java
@@ -0,0 +1,4 @@
+package com.nj.mcp.demo.mcpplayground.sec05.host.dto;
+
+public record ChatRequest(String message) {
+}
