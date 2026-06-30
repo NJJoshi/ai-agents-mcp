a9e84d32da74c2f9f50ed519eb9adaf0ef84453a
Nirav Joshi <joshi.nirav21@gmail.com>
2026-06-30 13:12:50 +0530

    MCP Tool: AI Agent build for ticket booking based on weather and ticket availability: host package classes and Spring boot main class

---
+++ mcp-playground/src/main/java/com/nj/mcp/demo/mcpplayground/sec06/SectionRunner.java
@@ -0,0 +1,54 @@
+package com.nj.mcp.demo.mcpplayground.sec06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class SectionRunner {
+
+	@SpringBootApplication(scanBasePackages = "com.nj.mcp.demo.mcpplayground.${section}.host")
+	static class Host {
+
+		static void main(String[] args) {
+			SpringApplication.run(Host.class, "--section=sec06", "--config=host");
+		}
+
+	}
+
+	@SpringBootApplication(scanBasePackages = "com.nj.mcp.demo.mcpplayground.${section}.servers.city")
+	static class CityService {
+
+		static void main(String[] args) {
+			SpringApplication.run(CityService.class,  "--section=sec06", "--config=city-service");
+		}
+
+	}
+
+	@SpringBootApplication(scanBasePackages = "com.nj.mcp.demo.mcpplayground.${section}.servers.flight")
+	static class FlightService {
+
+		static void main(String[] args) {
+			SpringApplication.run(FlightService.class,  "--section=sec06", "--config=flight-service");
+		}
+
+	}
+
+	@SpringBootApplication(scanBasePackages = "com.nj.mcp.demo.mcpplayground.${section}.servers.user")
+	static class UserService {
+
+		static void main(String[] args) {
+			SpringApplication.run(UserService.class,  "--section=sec06", "--config=user-service");
+		}
+
+	}
+
+	@SpringBootApplication(scanBasePackages = "com.nj.mcp.demo.mcpplayground.${section}.servers.weather")
+	static class WeatherService {
+
+		static void main(String[] args) {
+			SpringApplication.run(WeatherService.class,  "--section=sec06", "--config=weather-service");
+		}
+
+	}
+
+}
---
+++ mcp-playground/src/main/java/com/nj/mcp/demo/mcpplayground/sec06/host/dto/ChatRequest.java
@@ -0,0 +1,4 @@
+package com.nj.mcp.demo.mcpplayground.sec06.host.dto;
+
+public record ChatRequest(String message) {
+}
